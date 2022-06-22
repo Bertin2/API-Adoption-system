b4dbc98b4d233d2e64469a0350d1896098eef658
Bertin2 <bertinvenancio3526.321@gmail.com>
2022-06-22 17:19:55 -0500

    Primer commit

---
+++ database.js
@@ -0,0 +1,9 @@
+let config = {
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bffe824cd26b10",
+	password: "ddf8f9db",
+	port: 3306,
+	database: "heroku_f43ff55b6e92f6b",
+	secret_key:"V3nancio!"
+};
+exports.config = config;
