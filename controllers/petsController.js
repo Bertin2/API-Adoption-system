e7108e6bc531c0086b20baac9510e7997727318f
DiegoCaster <83787304+DiegoCaster@users.noreply.github.com>
2022-06-23 18:10:17 -0500

    API002-Pets Controller Método POST
    Implementación del metodo POST

--- controllers/petsController.js
+++ controllers/petsController.js
@@ -79,7 +79,7 @@ controller.getPets = (req, res) =>
     })
 }
 
-controller.postPet = (req, res) =>
+controller.postPet = (req, res) =>//post
 {
     const sql = "INSERT INTO pets SET ?";
     req.getConnection((error,conn) => {
